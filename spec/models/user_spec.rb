aa99e63c973f44c68b31479363eb04528d32c008
zjoe <zjoe546@gmail.com>
2013-12-14 21:37:30 +0800

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,7 +35,7 @@ describe User do
 
 	describe "when email format is invalid" do
 		it "should be invalid" do
-			addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+			addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com user@foo..com]
 			addresses.each do |invalid_address|
 				@user.email = invalid_address
 				expect(@user).not_to be_valid
@@ -50,6 +50,16 @@ describe User do
 				@user.email = valid_address
 				expect(@user).to be_valid
 			end
+		end
+	end
+
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
 		end
 	end
 
