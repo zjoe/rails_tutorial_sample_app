8a4453520aa697676cafd2cf1cc03ef690cd13b2
zjoe <zjoe546@gmail.com>
2013-12-18 21:55:05 +0800

    finishi exercises of chapter 9

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'http://ruby.taobao.org'
 
+ruby '2.0.0'
+
 gem 'rails', '4.0.0'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.0.1'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,11 @@ class UsersController < ApplicationController
 	end
 
 	def new
-	    @user = User.new
+		if signed_in?
+			redirect_to root_path
+		else
+			@user = User.new
+		end
 	end
 
 	def show
@@ -16,13 +20,17 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new(user_params)
-		if @user.save
-			sign_in @user
-			flash[:success] = "Welcome to the Sample App!"
-			redirect_to @user
+		if signed_in?
+			redirect_to root_path 
 		else
-		    render 'new'
+			@user = User.new(user_params)
+			if @user.save
+				sign_in @user
+				flash[:success] = "Welcome to the Sample App!"
+				redirect_to @user
+			else
+				render 'new'
+			end
 		end
 	end
 
@@ -47,7 +55,8 @@ class UsersController < ApplicationController
 	private
 
 		def user_params
-			params.require(:user).permit(:name, 
+			params.require(:user).permit(:admin, 
+										 :name, 
 										 :email, 
 										 :password, 
 										 :password_confirmation)
